2b208f79aa8865550dafc876734c826303c92318
Daniel <dtal@attentigroup.com>
2024-04-27 09:31:04 +0300

    sec07 - Deleting an Activity using MobX

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,29 +1,20 @@
 import React from 'react'
 import { Grid, GridColumn } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite'
-import { Activity } from '../../../app/models/activity'
 import ActivityList from './ActivityList'
 import ActivityDetails from './../details/ActivityDetails'
 import ActivityForm from '../form/ActivityForm'
 import { useStore } from '../../../app/stores/store'
 
-interface Props {
-    activities: Activity[];
-    deleteActivity: (is:string) => void;
-    submitting: boolean;
-}
 
-const ActivityDashboard = ({activities, deleteActivity, submitting }: Props) => {
+const ActivityDashboard = () => {
   const { activityStore } = useStore();
   const { selectedActivity, editMode } = activityStore;
 
   return (
     <Grid>
         <GridColumn width='10' >
-        <ActivityList 
-          activities={activities}
-          deleteActivity={deleteActivity}
-          submitting={submitting}/>
+        <ActivityList />
         </GridColumn>
         <GridColumn width='6'>
           { selectedActivity && !editMode && 
--- Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,24 +1,20 @@
 import React, { SyntheticEvent, useState } from 'react'
 import { Button, Item, Label, Segment } from 'semantic-ui-react'
+import { observer } from 'mobx-react-lite';
 import { Activity } from '../../../app/models/activity'
 import { useStore } from '../../../app/stores/store';
 
-interface Props {
-    activities: Activity[];
-    deleteActivity: (is:string) => void;
-    submitting: boolean;
-}
 
-const ActivityList = ({activities, deleteActivity, submitting}: Props) => {
+const ActivityList = () => {
+  const { activityStore } = useStore();
+  const { activities, deleteActivity, loading } = activityStore;
+
   const [ target, setTarget] = useState('');
 
   function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
     setTarget(e.currentTarget.name);
     deleteActivity(id);
   }
-
-  const { activityStore } = useStore();
-
 
   return (
     <Segment>
@@ -36,7 +32,7 @@ const ActivityList = ({activities, deleteActivity, submitting}: Props) => {
                 <Button onClick={()=>activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue'/>
                 <Button 
                   name={activity.id}
-                  loading={submitting && target === activity.id}
+                  loading={loading && target === activity.id}
                   onClick={(e)=>handleActivityDelete(e, activity.id)} floated='right' content='Delete' color='red'/>
                 <Label basic content={activity.category}/>
               </Item.Extra>
@@ -48,4 +44,4 @@ const ActivityList = ({activities, deleteActivity, submitting}: Props) => {
   )
 }
 
-export default ActivityList
+export default observer(ActivityList);
