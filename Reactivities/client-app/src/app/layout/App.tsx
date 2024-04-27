2cf9e015e36244af6f78ed639e3ecf1b9a073daa
Daniel <dtal@attentigroup.com>
2024-04-27 11:00:12 +0300

    sec08 - adding routes & Outlet

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,25 +1,15 @@
-import { useEffect } from 'react'
 import { Container } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
+import { Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities()
-  }, [activityStore])
-
-  if(activityStore.loadingInitial) return <LoadingComponent content='Loading App' />
 
   return (
     <>
       <NavBar/>
       <Container style={{marginTop: '7em'}}>
-      <ActivityDashboard />
+      <Outlet />
       </Container>
     </>
   )
--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid, GridColumn } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite'
 import ActivityList from './ActivityList'
 import ActivityDetails from './../details/ActivityDetails'
 import ActivityForm from '../form/ActivityForm'
 import { useStore } from '../../../app/stores/store'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 
 
 const ActivityDashboard = () => {
   const { activityStore } = useStore();
   const { selectedActivity, editMode } = activityStore;
+
+  useEffect(() => {
+    activityStore.loadActivities()
+  }, [activityStore])
+
+  if(activityStore.loadingInitial) return <LoadingComponent content='Loading App' />
 
   return (
     <Grid>
