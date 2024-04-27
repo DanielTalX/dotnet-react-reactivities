ee9e718d67f4704cb545b3751f199c864d134372
Daniel <dtal@attentigroup.com>
2024-04-27 10:21:40 +0300

    sec07 - using a Map to store the activities

--- Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -7,7 +7,7 @@ import { useStore } from '../../../app/stores/store';
 
 const ActivityList = () => {
   const { activityStore } = useStore();
-  const { activities, deleteActivity, loading } = activityStore;
+  const { activitiesByDate, deleteActivity, loading } = activityStore;
 
   const [ target, setTarget] = useState('');
 
@@ -19,7 +19,7 @@ const ActivityList = () => {
   return (
     <Segment>
       <Item.Group divided>
-        { activities.map(activity => (
+        { activitiesByDate.map(activity => (
           <Item key={activity.id}>
             <Item.Content>
               <Item.Header as='a'>{activity.title}</Item.Header>
